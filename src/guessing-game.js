292b7e19dde79dbfff70054222d9feb835a93dd8
Elena Sinelle <elle.sinelle@gmail.com>
2022-08-16 17:01:15 +0300

    feat: task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        while (this.min <= this.max) {
+            this.guessed = Math.round((this.min + this.max) / 2 );
+            return this.guessed;
+        }
     }
 
     lower() {
-
+        if (this.guessed < this.max) {
+            this.max = this.guessed;
+        }
     }
 
     greater() {
-
+        if (this.guessed > this.min) {
+            this.min = this.guessed;
+        }
     }
 }
 
